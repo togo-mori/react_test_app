841e4e9696e1ad7e28525b9313c69d7ab6b56de3
togo-mori <togo.mori@divx.co.jp>
2022-03-30 16:41:31 +0900

    lint executed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import QRCode from 'qrcode.react';
-import { Print } from './components/users/Print';
-import { Profile } from './components/users/Profile';
+import React from "react"
+import "./App.css"
+import QRCode from "qrcode.react"
+import { Print } from "./components/users/Print"
+import { Profile } from "./components/users/Profile"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Profile />
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Profile />
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/users/Preview/index.tsx
+++ src/components/users/Preview/index.tsx
@@ -1,50 +1,50 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
-import React, { useState } from 'react';
-import { useRef } from 'react';
-import ReactToPrint from 'react-to-print';
-import ComponentToPrint from '../ComponentToPrint';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material"
+import React, { useState } from "react"
+import { useRef } from "react"
+import ReactToPrint from "react-to-print"
+import ComponentToPrint from "../ComponentToPrint"
 
 export const Preview = () => {
-  const ref = useRef(null);
-  const [open, setOpen] = useState<boolean>(false);
+	const ref = useRef(null)
+	const [open, setOpen] = useState<boolean>(false)
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+	const handleClickOpen = () => {
+		setOpen(true)
+	}
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false)
+	}
 
-  return (
-    <div>
-      <Button size="small" color="primary" onClick={handleClickOpen}>
+	return (
+		<div>
+			<Button size="small" color="primary" onClick={handleClickOpen}>
         プレビュー
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{`testのプロフィールを印刷しますか？`}</DialogTitle>
-        <DialogContent>
-          <ComponentToPrint ref={ref} />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
+			</Button>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+			>
+				<DialogTitle id="alert-dialog-title">{"testのプロフィールを印刷しますか？"}</DialogTitle>
+				<DialogContent>
+					<ComponentToPrint ref={ref} />
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color="primary">
             キャンセル
-          </Button>
-          <ReactToPrint
-            trigger={() => (
-              <Button size="small" color="primary">
+					</Button>
+					<ReactToPrint
+						trigger={() => (
+							<Button size="small" color="primary">
                 印刷
-              </Button>
-            )}
-            content={() => ref.current}
-          />
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
-};
+							</Button>
+						)}
+						content={() => ref.current}
+					/>
+				</DialogActions>
+			</Dialog>
+		</div>
+	)
+}
